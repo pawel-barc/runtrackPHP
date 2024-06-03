40dbec7e787238dd38eeb5e3b2dee8e86b856dbb
Pawel Barc <pawel.barc@laplateforme.io>
2024-06-03 11:57:29 +0200

    Le job 01 terminé

---
+++ jour01/job01/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $var1 = 'LaPlateforme';
+    $var2 = ' Vive ';
+    $var3 = '!';
+    echo $var2, $var1, ' ', $var3
+
+    ?>
+    <br>
+    <?php
+    $chiffre1 = 6;
+    echo $chiffre1 . '<br>';
+    echo $chiffre1 + 4;
+    echo '<br>';
+    $greenmilk = false;
+    echo $greenmilk;
+    echo '<br>';
+    $greenmilk = true;
+    echo $greenmilk;
+    ?>
+</body>
+</html>
