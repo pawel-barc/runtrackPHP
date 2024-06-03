7043eb4c684bf9ec73054613664d3795b1bda451
Pawel Barc <pawel.barc@laplateforme.io>
2024-06-03 14:29:49 +0200

    Les jobs 04 et 05 terminé

---
+++ jour01/job04/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    for ($i = 0; $i<1337; $i++){
+        if ($i == 42){
+            echo "<strong><u>$i</u></strong><br>";
+        } else {
+            echo "$i<br>";
+        }
+    }
+    ?>
+</body>
+</html>
---
+++ jour01/job05/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    for ($i = 0; $i < 1337; $i++){
+        if ($i != 26 && $i != 37 & $i != 88 & $i != 111) {
+            echo "$i<br>";
+        }
+    }
+    ?>
+
+</body>
+</html>
