e5daa72a6ba1b04173120f7bb0644030f5b9c84e
Pawel Barc <pawel.barc@laplateforme.io>
2024-06-06 14:11:42 +0200

    Les jobs 01 et 02 du jour04 terminés

---
+++ jour04/job01/index.php
@@ -0,0 +1,28 @@
+
+<?php
+session_start();
+if (isset($_POST['reset'])){
+    $_SESSION['nbVisites'] = 0;
+}else{
+    if(isset($_SESSION['nbVisites'])){
+        $_SESSION['nbVisites'] +=1;
+    }else{
+        $_SESSION['nbVisites'] =1;
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Compteur de visites</title>
+</head>
+<body>
+    <p>Nombre de visites : <?php echo $_SESSION['nbVisites']; ?></p>
+    <form method="post" >
+        <button type="submit" name="reset">Reset</button>
+    </form>
+</body>
+</html>
---
+++ jour04/job02/index.php
@@ -0,0 +1,27 @@
+<?php
+if(isset($_POST['reset'])){
+    setcookie("nbVisites", 0, time() -3600);
+    $nbVisites =0;
+}else{
+    if(isset($_POST["nbVisites"])){
+        $nbVisites = $_COOKIE["nbVisites"] + 1;
+    }else{
+        $nbVisites = 0;
+    }
+    setcookie("nbVisites", $nbVisites, time() + 3600 );
+}
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Compteur de visites</title>
+</head>
+<body>
+    <p>Nombres de visites : <?php echo $nbVisites; ?></p>
+    <form method "post">
+        <button type "submit" name "reset">Reset</button>
+    </form>    
+</body>
+</html>
